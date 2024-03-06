c127a92903e08af4379a165bada21f48ad1f5b32
rutvikpatel122 <pr3179217@gmail.com>
2024-03-06 20:50:07 +0530

    project detail page ui change commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "ce365a2f9c22ea20d0940d63b8b641dd",
-"assets/AssetManifest.bin.json": "5695d32a242ea37da4dc2310385b4b9b",
-"assets/AssetManifest.json": "0d99780b254a2f97b951c77cff57d731",
+const RESOURCES = {"assets/AssetManifest.bin": "8f687da2a14a00e25ec37e95a2aa0d54",
+"assets/AssetManifest.bin.json": "32666bc94fbfe6dc95cccb2bb01a7e75",
+"assets/AssetManifest.json": "7c42b50c35df765530a922151b1ac1be",
 "assets/assets/fonts/Raleway-Black.ttf": "ca057783cb90d483d16662f202212050",
 "assets/assets/fonts/Raleway-BlackItalic.ttf": "6bb69e40367ae9814db8c80152eed900",
 "assets/assets/fonts/Raleway-Bold.ttf": "21c82294041b1504a5cbe4f566c8acd6",
@@ -24,13 +24,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "ce365a2f9c22ea20d0940d63b8b641dd
 "assets/assets/fonts/Raleway-SemiBoldItalic.ttf": "5890daae80f0d711900058b808f819ec",
 "assets/assets/fonts/Raleway-Thin.ttf": "02cf7464c6ce00474e8d90b69f103d75",
 "assets/assets/fonts/Raleway-ThinItalic.ttf": "73456336793992177ea8a337ccaa6950",
+"assets/assets/images/appstore.png": "20be9e63f99f0c35ba3e97ae378e2848",
 "assets/assets/images/back.png": "7b8b02655de4538068c4496650c15ffe",
 "assets/assets/images/clear.png": "b9ff32e40f486bee679f3860ed929ea6",
 "assets/assets/images/logo.png": "51a763386728c05d71df117f5e2a213c",
 "assets/assets/images/menu.png": "8e585940667b6edda0d6e5d09484339b",
+"assets/assets/images/playstore.png": "707c1bbb8db16e93a85c3b302c1bc077",
 "assets/assets/images/profilePic.png": "c7b8c92a70cae79da69d36bc68c99110",
+"assets/assets/images/web.png": "1ee7ffac0b8d7bbd60b85c56757650b8",
 "assets/FontManifest.json": "62f53e3a77f0bd88351b3c5210ae129e",
-"assets/fonts/MaterialIcons-Regular.otf": "f9eabb0b7c76ef31c5e1f1af253e1b6a",
+"assets/fonts/MaterialIcons-Regular.otf": "6bcd8780408e8fee5a5709b772c7fde9",
 "assets/NOTICES": "570701c5c9e8ddd86033a563e02d621b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "4096b5150bac93c41cbc9b45276bd90f",
@@ -47,9 +50,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "ce365a2f9c22ea20d0940d63b8b641dd
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4a58b42be6a236a6af6404a7fa6c63ac",
-"/": "4a58b42be6a236a6af6404a7fa6c63ac",
-"main.dart.js": "7ba0886e4aa17b6ebfc37da82be4e23c",
+"index.html": "a17e53d62839b98f958ce306f9ff1bb7",
+"/": "a17e53d62839b98f958ce306f9ff1bb7",
+"main.dart.js": "05ec9680fe3e98402cb1505a6d267ba9",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"};
 // The application shell files that are downloaded before a service worker can
