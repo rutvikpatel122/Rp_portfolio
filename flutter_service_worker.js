2cf955e908c560b51247c9109b41510346a9458b
rutvikpatel122 <pr3179217@gmail.com>
2024-06-27 15:45:11 +0530

    image error fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,9 +53,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "8f687da2a14a00e25ec37e95a2aa0d54
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e413e6c940e623512fb1625d1d7e4537",
-"/": "e413e6c940e623512fb1625d1d7e4537",
-"main.dart.js": "6e767a30abaa9676a6e86f1827016e09",
+"index.html": "82059efb26238eb6acc9ab93968e1140",
+"/": "82059efb26238eb6acc9ab93968e1140",
+"main.dart.js": "ec76306347811d5bc1cdf96afcbef03a",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"};
 // The application shell files that are downloaded before a service worker can
